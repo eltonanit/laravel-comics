78128336695544042017a7806a390ca77cb945d0
eltonanit <elton.ani@gmail.com>
2024-09-25 17:52:57 +0200

    [UPD]Added header-menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,11 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    @yield('content')
+    @include('partials.header')
+    <main>
+      @yield('content')  
+    </main>
+    
     
 </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,23 @@
         <div class="row">
             <div class="col-12">
                 <div class="d-flex justify-content-between">
-                    <div class="logo"></div>
-                    <div class="menu"></div>
+                    <div class="logo">
+                        <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
+                    </div>
+                    <div class="menu">
+                        <ul class="list-unstyled d-flex">
+                            <li><a href="#" class="nav-link">Charachters</a></li>
+                            <li><a href="#" class="nav-link">Comics</a></li>
+                            <li><a href="#" class="nav-link">Movies</a></li>
+                            <li><a href="#" class="nav-link">TV</a></li>
+                            <li><a href="#" class="nav-link">GAmes</a></li>
+                            <li><a href="#" class="nav-link">Collectibles</a></li>
+                            <li><a href="#" class="nav-link">Videos</a></li>
+                            <li><a href="#" class="nav-link">Fans</a></li>
+                            <li><a href="#" class="nav-link">News</a></li>
+                            <li><a href="#" class="nav-link">Shop</a></li>
+                        </ul>
+                    </div>
                     <div class="search"></div>
                 </div>
             </div>
