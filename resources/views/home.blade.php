9f90191bcc6070d77bfa9812d6c4ca8b7bfa8b2a
eltonanit <elton.ani@gmail.com>
2024-09-25 17:29:51 +0200

    [UPD]Added header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="jumbotron">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h2>JUMBOTRON</h2>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            SPAZIO FUMETTI
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between">
+                    <div class="logo"></div>
+                    <div class="menu"></div>
+                    <div class="search"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
