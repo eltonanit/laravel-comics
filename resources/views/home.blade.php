fe55c11ceb51128883e41448ca752bcb0508f6c1
eltonanit <elton.ani@gmail.com>
2024-09-27 19:03:32 +0200

    [FIX] fixed css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,19 +11,21 @@
         </div>
     </div>
 </div>
-<div class="container">
-    <div class="comics-container">
+<div id="comics-container">
+    <div class="container">
         <div class="row">
         <div class="col-12">
             SPAZIO FUMETTI
         </div>
+        </div>
+         
         <div class="row">
             @foreach($comics as $comic)
             <div class="col-12 col-md-3 col-lg-2">
-                <div class="card border-0">
-                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                <div class="comic-card border-0 m-1">
+                    <img class="img-fluid" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                     <div class="card-body">
-                        {{ $comic['title'] }}
+                      <span clas="text-uppercase">  {{ $comic['title'] }} </span> 
                     </div>
 
                 </div>
@@ -34,5 +36,5 @@
     </div>
 </div>
     
-</div>
+
 @endsection
