3afc108ce9edd31615af4c546150a6d2a82e3b8d
eltonanit <elton.ani@gmail.com>
2024-09-27 17:16:21 +0200

    [UPD] Added array card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,10 +12,27 @@
     </div>
 </div>
 <div class="container">
-    <div class="row">
+    <div class="comics-container">
+        <div class="row">
         <div class="col-12">
             SPAZIO FUMETTI
         </div>
+        <div class="row">
+            @foreach($comics as $comic)
+            <div class="col-12 col-md-3 col-lg-2">
+                <div class="card border-0">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    <div class="card-body">
+                        {{ $comic['title'] }}
+                    </div>
+
+                </div>
+
+            </div>
+            @endforeach
+        </div>
     </div>
+</div>
+    
 </div>
 @endsection
